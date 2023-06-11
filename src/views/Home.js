04376b77edaa0ac72378afb9e90ebfdb42f6b26d
jcartronics <jcar-95@hotmail.com>
2023-06-11 14:49:26 -0400

    Agrega maqueta Home y estilos

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <h1 className='title'>Bienvenido a <strong>Happy Cake</strong></h1>
+        <p className='sub-title'>El lugar de los pasteles felices</p>
+        <i className='home-logo'>🎂</i>
+    </div>
   )
 }
