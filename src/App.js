3c6ecd3732d82ad1cf9d0cc4bd87e9f0f644895c
jcartronics <jcar-95@hotmail.com>
2023-06-11 14:59:20 -0400

    Agrega React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './views/Home';
+import { Contacto } from './views/Contacto';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
+      <BrowserRouter>
+        <Navbar/>
+        
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/contacto' element={<Contacto/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
