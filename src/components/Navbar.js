288b16da499d508d5161449a39f43fb961a81d39
jcartronics <jcar-95@hotmail.com>
2023-06-11 14:37:24 -0400

    Agrega maqueta de Navbar y estilo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <ul className='nav-list'>
+            <li>
+                <a href="../views/Home"><i>🏠</i>Home</a>
+            </li>
+            <li>
+                <a href="../views/Contacto"><i>📒</i>Contacto</a>
+            </li>
+        </ul>
+        <div className="logo">
+            <a>Happy Cake</a>
+            <i>🎂</i>
+        </div>
+    </div>
   )
 }
